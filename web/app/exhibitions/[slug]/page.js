0d67629888015594e1123f9f5b76022b58324bd6
Mitchell Barton <mitchell@welikesmall.com>
2023-12-08 11:47:17 -0700

    metadata

--- web/app/exhibitions/[slug]/page.js
+++ web/app/exhibitions/[slug]/page.js
@@ -1,5 +1,6 @@
 import { sanityClient } from '@/lib/sanity'
 import ExhibitionContent from '@/components/ExhibitionContent'
+import { urlFor } from '@/lib/sanity'
 
 async function getData(slug) {
   const exhibition = await sanityClient.fetch(`*[_type == "exhibition" && slug.current == $slug][0]{
@@ -42,6 +43,32 @@ async function getData(slug) {
   })
 
   return exhibition
+}
+
+export async function generateMetadata({ params }) {
+  const { slug } = params
+
+  const res = await sanityClient.fetch(`*[_type == "exhibition" && slug.current == $slug][0]{
+    title,
+    ogImage{
+      asset->{
+        ...,
+        metadata
+      }
+    }
+  }`, { slug }, {
+    cache: 'no-store'
+  })
+
+  const exhibitionData = res.result
+  const image = urlFor(exhibitionData.ogImage.asset.url).width(1500).quality(80).url()
+ 
+  return {
+    title: `Washer / Dryer Projects | ${exhibitionData.title}`,
+    openGraph: {
+      images: [image]
+    }
+  }
 }
 
 export default async function ExhibitionDetail({ params }) {
