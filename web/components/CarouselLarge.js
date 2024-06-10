244ce7e90815854cd3276c618febeeee0981cc89
Mitchell Barton <mitchell@welikesmall.com>
2024-06-10 10:10:25 -0600

    captions in carousel

--- web/components/CarouselLarge.js
+++ web/components/CarouselLarge.js
@@ -6,6 +6,7 @@ import 'swiper/css'
 import { useRef, useEffect, useState } from 'react'
 import { urlFor, getImageHeight } from '@/lib/sanity'
 import DefImage from '@/components/DefImage'
+import { PortableText } from '@portabletext/react'
 
 const CarouselLarge = ({ files, className, setCarouselState, currentCarouselItem }) => {
   const swiperRef = useRef(null)
@@ -21,26 +22,40 @@ const CarouselLarge = ({ files, className, setCarouselState, currentCarouselItem
   return (
     <div className={`carousel-large fixed top-0 left-0 w-full h-full bg-white flex flex-col ${className}`}>
       <div className="px-6 lg:px-8 py-6 flex-none flex justify-between items-center">
-        <div className="flex items-center">
+        <div className="flex items-start">
           <div className="flex items-center">
             <button
               onClick={() => swiperRef.current.swiper.slidePrev()}
               className="underline lg:hover:no-underline mr-4"
-            >Prev</button>
+            >
+              Prev
+            </button>
 
             <button
               onClick={() => swiperRef.current.swiper.slideNext()}
               className="underline lg:hover:no-underline"
-            >Next</button>
+            >
+              Next
+            </button>
           </div>
 
-          <div className="ml-8">{currentSlide + 1} / {files.length}</div>
+          <div className="ml-8">
+            {currentSlide + 1} / {files.length}
+          </div>
+
+          {files[currentSlide].text && (
+            <div className="ml-16 hidden lg:block">
+              <PortableText value={files[currentSlide].text} />
+            </div>
+          )}
         </div>
 
         <button
           onClick={() => setCarouselState(false)}
           className="underline lg:hover:no-underline"
-        >Close</button>
+        >
+          Close
+        </button>
       </div>
 
       <div className="flex-1 h-full relative p-6 lg:p-12">
@@ -53,14 +68,14 @@ const CarouselLarge = ({ files, className, setCarouselState, currentCarouselItem
             className="!fill-parent"
             onSlideChange={(swiper) => setCurrentSlide(swiper.realIndex)}
           >
-            {files.map((file, index) => { 
+            {files.map((file, index) => {
               if (file._type === 'imageObject') {
                 return (
                   <SwiperSlide key={index}>
                     <div className="w-full h-full flex justify-center items-center">
-                      <DefImage 
-                        src={urlFor(file.imageFile.asset.url).width(1500).quality(80).url()} 
-                        width={1500} 
+                      <DefImage
+                        src={urlFor(file.imageFile.asset.url).width(1500).quality(80).url()}
+                        width={1500}
                         height={getImageHeight(1500, file.imageFile.asset.metadata.dimensions.aspectRatio)}
                         className="w-full h-full object-contain"
                         alt={file.imageFile.alt}
